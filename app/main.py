4d97dc337779c62617af6dc12f83c3884886f1da
patelchaitany <patelchaitany93@gmail.com>
2025-08-19 22:04:29 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ import sys
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
+    if chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1]) == "e":
+        return int(bencoded_value[1:-1])
     if chr(bencoded_value[0]).isdigit():
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
